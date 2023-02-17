bd03f226ba9b51047c6bb697c64181ce538ef436
Zunawe <gyroscope15@gmail.com>
2023-02-16 22:11:12 -0800

    Be a lot smarter about extracting data and actually use the sym file

--- tools/extractor/extractor.cpp
+++ tools/extractor/extractor.cpp
@@ -9,10 +9,6 @@
 #include <json.hpp>
 using json = nlohmann::json;
 
-std::regex create_regex_for_symbol (std::string symbol) {
-    return std::regex("^[ ]*(0x[0-9a-fA-F]+)[ ]+" + symbol + ".*$");
-}
-
 int main (int argc, char *argv[])
 {
     std::filesystem::path root_dir;
@@ -31,8 +27,8 @@ int main (int argc, char *argv[])
         exit(1);
     }
 
-    std::vector<std::shared_ptr<BallItemInfo>> ball_items;
-    std::vector<std::shared_ptr<HiddenItemInfo>> hidden_items;
+    std::vector<std::shared_ptr<ItemInfo>> ball_items;
+    std::vector<std::shared_ptr<ItemInfo>> hidden_items;
 
     std::ifstream macro_file(root_dir / "tools/extractor/macros.json");
     if (macro_file.fail())
@@ -41,6 +37,21 @@ int main (int argc, char *argv[])
         exit(1);
     }
     json macros_json = json::parse(macro_file);
+
+    std::cout << "Looking up symbols..." << std::endl;
+
+    std::ifstream symbol_map_file(root_dir / "pokeemerald.sym");
+    std::map<std::string, uint32_t> symbol_map;
+    std::regex symbol_map_regex("^([0-9a-fA-F]+) [lg] [0-9a-fA-F]+ ([a-zA-Z0-9_]+)$");
+    std::string line;
+    while (std::getline(symbol_map_file, line))
+    {
+        std::smatch m;
+        if (std::regex_match(line, m, symbol_map_regex))
+        {
+            symbol_map[m[2]] = std::stoi(m[1], nullptr, 16);
+        }
+    }
 
     std::cout << "Reading map files..." << std::endl;
 
@@ -52,56 +63,107 @@ int main (int argc, char *argv[])
             json map_data_json = json::parse(map_file);
 
             json object_events_json = map_data_json["object_events"];
-            for (const auto& event: object_events_json) {
-                if (event["flag"].get<std::string>().substr(0, 9) == "FLAG_ITEM")
+            for (const auto& event_json: object_events_json) {
+                if (event_json["flag"].get<std::string>().substr(0, 9) == "FLAG_ITEM")
                 {
-                    std::shared_ptr<BallItemInfo> item(new BallItemInfo());
-                    item->script_name = event["script"].get<std::string>();
-                    item->script_symbolmap_regex = create_regex_for_symbol(item->script_name);
-                    item->flag_name = event["flag"].get<std::string>();
+                    std::shared_ptr<ItemInfo> item(new ItemInfo());
+                    item->flag_name = event_json["flag"];
+                    item->ram_address = symbol_map[event_json["script"]] + 3;
+                    item->rom_address = item->ram_address - 0x8000000;
                     ball_items.push_back(item);
                 }
             }
 
             json bg_events_json = map_data_json["bg_events"];
-            for (const auto& event: bg_events_json) {
-                if (event["type"].get<std::string>() == "hidden_item")
+            for (const auto& event_json: bg_events_json) {
+                if (event_json["type"] == "hidden_item")
                 {
-                    std::shared_ptr<HiddenItemInfo> item(new HiddenItemInfo());
-                    item->flag_name = event["flag"].get<std::string>();
-                    item->item_symbolmap_regex = create_regex_for_symbol("Archipelago_Target_" + item->flag_name);
-                    item->default_item = macros_json["items"][event["item"].get<std::string>()];
+                    std::shared_ptr<ItemInfo> item(new ItemInfo());
+                    item->flag_name = event_json["flag"];
+                    item->ram_address = symbol_map["Archipelago_Target_" + item->flag_name] + 8;
+                    item->rom_address = item->ram_address - 0x8000000;
+                    item->default_item = macros_json["items"][event_json["item"].get<std::string>()];
                     hidden_items.push_back(item);
                 }
             }
         }
     }
 
-    std::cout << "Looking up symbols..." << std::endl;
+    std::cout << "Reading wild encounters..." << std::endl;
 
-    std::ifstream symbol_map(root_dir / "pokeemerald.map");
-    std::string line;
-    while (std::getline(symbol_map, line))
-    {
-        for (const auto& item: ball_items)
+    std::vector<std::shared_ptr<EncounterTableInfo>> encounter_tables;
+
+    std::ifstream wild_encounters_file(root_dir / "src/data/wild_encounters.json");
+    json wild_encounters_json = json::parse(wild_encounters_file);
+    
+    for (const auto& table_json: wild_encounters_json["wild_encounter_groups"][0]["encounters"]) {
+        std::shared_ptr<EncounterTableInfo> table(new EncounterTableInfo());
+        std::string base_symbol = table_json["base_label"];
+
+        table->land_pokemon_ram_address = symbol_map[base_symbol + "_LandMons"];
+        table->water_pokemon_ram_address = symbol_map[base_symbol + "_WaterMons"];
+        table->fishing_pokemon_ram_address = symbol_map[base_symbol + "_FishingMons"];
+
+        table->map_name = table_json["map"];
+
+        uint i;
+
+        i = 0;
+        try
         {
-            std::smatch m;
-            if (std::regex_match(line, m, item->script_symbolmap_regex))
-            {
-                item->ram_address = std::stoi(m[1], nullptr, 16) + 3;
-                item->rom_address = item->ram_address - 0x8000000;
+            for (const auto& encounter_slot_json: table_json.at("land_mons")["mons"]) {
+                table->land_encounters[i].default_species = macros_json["species"][encounter_slot_json["species"].get<std::string>()];
+                table->land_encounters[i].min_level = encounter_slot_json["min_level"];
+                table->land_encounters[i].max_level = encounter_slot_json["max_level"];
+                ++i;
+            }
+            table->land_encounters_exist = true;
+        }
+        catch (const json::exception &e)
+        {
+            if (e.id != 403) {
+                throw e;
             }
         }
-        
-        for (const auto& item: hidden_items)
+
+        i = 0;
+        try
         {
-            std::smatch m;
-            if (std::regex_match(line, m, item->item_symbolmap_regex))
-            {
-                item->ram_address = std::stoi(m[1], nullptr, 16) + 8;
-                item->rom_address = item->ram_address - (0x8000000 - 0x30); // Not sure why there's an extra 0x30 offset
+            table_json.at("water_mons");
+            for (const auto& encounter_slot_json: table_json.at("water_mons")["mons"]) {
+                table->water_encounters[i].default_species = macros_json["species"][encounter_slot_json["species"].get<std::string>()];
+                table->water_encounters[i].min_level = encounter_slot_json["min_level"];
+                table->water_encounters[i].max_level = encounter_slot_json["max_level"];
+                ++i;
+            }
+            table->water_encounters_exist = true;
+        }
+        catch (const json::exception &e)
+        {
+            if (e.id != 403) {
+                throw e;
             }
         }
+
+        i = 0;
+        try
+        {
+            for (const auto& encounter_slot_json: table_json.at("fishing_mons")["mons"]) {
+                table->fishing_encounters[i].default_species = macros_json["species"][encounter_slot_json["species"].get<std::string>()];
+                table->fishing_encounters[i].min_level = encounter_slot_json["min_level"];
+                table->fishing_encounters[i].max_level = encounter_slot_json["max_level"];
+                ++i;
+            }
+            table->fishing_encounters_exist = true;
+        }
+        catch (const json::exception &e)
+        {
+            if (e.id != 403) {
+                throw e;
+            }
+        }
+
+        encounter_tables.push_back(table);
     }
 
     std::cout << "Reading ROM..." << std::endl;
@@ -138,11 +200,47 @@ int main (int argc, char *argv[])
         });
     }
 
+    json encounter_tables_json;
+    uint i = 0;
+    for (const auto& table: encounter_tables)
+    {
+        encounter_tables_json.push_back({
+            { "map_name", table->map_name },
+        });
+        
+        if (table->land_encounters_exist) {
+            encounter_tables_json[i]["land_pokemon_ram_address"] = table->land_pokemon_ram_address;
+            encounter_tables_json[i]["land_encounters"] = json::array();
+            for (uint j = 0; j < 12; ++j) {
+                encounter_tables_json[i]["land_encounters"].push_back(table->land_encounters[j].default_species);
+            }
+        }
+
+        if (table->water_encounters_exist) {
+            encounter_tables_json[i]["water_pokemon_ram_address"] = table->water_pokemon_ram_address;
+            encounter_tables_json[i]["water_encounters"] = json::array();
+            for (uint j = 0; j < 5; ++j) {
+                encounter_tables_json[i]["water_encounters"].push_back(table->water_encounters[j].default_species);
+            }
+        }
+
+        if (table->fishing_encounters_exist) {
+            encounter_tables_json[i]["fishing_pokemon_ram_address"] = table->fishing_pokemon_ram_address;
+            encounter_tables_json[i]["fishing_encounters"] = json::array();
+            for (uint j = 0; j < 10; ++j) {
+                encounter_tables_json[i]["fishing_encounters"].push_back(table->fishing_encounters[j].default_species);
+            }
+        }
+
+        ++i;
+    }
+
     json output_json = {
         { "ball_items", ball_items_json },
         { "hidden_items", hidden_items_json },
+        { "encounter_tables", encounter_tables_json },
         { "items", macros_json["items"] },
-        { "flags", macros_json["flags"] }
+        { "flags", macros_json["flags"] },
     };
 
     std::ofstream outfile(root_dir / "data.json");
--- tools/extractor/extractor.h
+++ tools/extractor/extractor.h
@@ -2,25 +2,34 @@
 #define EXTRACTOR_H
 
 #include <string>
-#include <regex>
 
-struct BallItemInfo {
-    public:
-        std::string script_name;
-        std::regex script_symbolmap_regex;
-        uint32_t ram_address;
-        uint32_t rom_address;
-        std::string flag_name;
-        uint8_t default_item;
+struct ItemInfo {
+    uint32_t ram_address;
+    uint32_t rom_address;
+    std::string flag_name;
+    uint16_t default_item;
 };
 
-struct HiddenItemInfo {
-    public:
-        std::regex item_symbolmap_regex;
-        uint32_t ram_address;
-        uint32_t rom_address;
-        std::string flag_name;
-        uint8_t default_item;
+struct EncounterSlotInfo {
+    uint16_t default_species;
+    uint8_t min_level;
+    uint8_t max_level;
+};
+
+struct EncounterTableInfo {
+    std::string map_name;
+    uint32_t land_pokemon_ram_address;
+    uint32_t land_pokemon_rom_address;
+    uint32_t water_pokemon_ram_address;
+    uint32_t water_pokemon_rom_address;
+    uint32_t fishing_pokemon_ram_address;
+    uint32_t fishing_pokemon_rom_address;
+    bool land_encounters_exist;
+    bool water_encounters_exist;
+    bool fishing_encounters_exist;
+    EncounterSlotInfo land_encounters[12];
+    EncounterSlotInfo water_encounters[5];
+    EncounterSlotInfo fishing_encounters[10];
 };
 
 #endif
