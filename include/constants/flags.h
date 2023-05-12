4c393496bb93ea5432f2230ca19729ba4fb17eaa
Lucas Towers <luctowers@gmail.com>
2023-05-11 17:04:12 -0700

    Switch to dummy flags for latios and latias

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -41,8 +41,10 @@
 #define FLAG_TEMP_1F     (TEMP_FLAGS_START + 0x1F)
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
+// TODO: fix these for legendary hunt goal
+#define FLAG_DUMMY_LATIOS    0x20
+#define FLAG_DUMMY_LATIAS    0x21
+
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
